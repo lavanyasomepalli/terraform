7c53e4fb04d22046ed944f55dc4b9d42abd71b70
root <root@ip-172-31-84-21.ec2.internal>
2024-11-13 05:59:37 +0000

    adding terraform repo

---
+++ subnet.tf
@@ -0,0 +1,53 @@
+resource "aws_subnet" "public-subnet_1" {
+vpc_id = "${aws_vpc.demovpc.id}"
+cidr_block = "10.0.1.0/24"
+map_public_ip_on_launch = true
+availability_zone = "us-east-1a"
+tags = {
+Name = "web subnet 1"
+}
+}
+resource "aws_subnet" "public-subnet_2" {
+vpc_id = "${aws_vpc.demovpc.id}"
+cidr_block = "10.0.2.0/24"
+map_public_ip_on_launch = true
+availability_zone = "us-east-1b"
+tags = {
+Name = "web subnet 2"
+}
+}
+resource "aws_subnet" "application-subnet_1" {
+vpc_id = "${aws_vpc.demovpc.id}"
+cidr_block = "10.0.3.0/24"
+map_public_ip_on_launch = false
+availability_zone = "us-east-1a"
+tags = {
+Name = "application subnet 1"
+}
+}
+resource "aws_subnet" "application-subnet_2" {
+vpc_id = "${aws_vpc.demovpc.id}"
+cidr_block = "10.0.4.0/24"
+map_public_ip_on_launch = false
+availability_zone = "us-east-1b"
+tags = {
+Name = "application subnet 2"
+}
+}
+resource "aws_subnet" "database-subnet_1" {
+vpc_id = "${aws_vpc.demovpc.id}"
+cidr_block = "10.0.5.0/24"
+availability_zone = "us-east-1a"
+tags = {
+Name = "Database subnet 1"
+}
+}
+resource "aws_subnet" "database-subnet_2" {
+vpc_id = "${aws_vpc.demovpc.id}"
+cidr_block = "10.0.6.0/24"
+availability_zone = "us-east-1b"
+tags = {
+Name = "Database subnet 2"
+}
+}
+
